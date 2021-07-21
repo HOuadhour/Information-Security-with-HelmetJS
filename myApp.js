bb96858163dac5c8eaf9437f58143e0c3f437bcb
HOuadhour <HOuadhour@Yandex.com>
2021-07-21 22:24:51 +0100

    Configure Helmet Using the ‘parent’ helmet() Middleware

--- myApp.js
+++ myApp.js
@@ -5,16 +5,32 @@ const app = express();
 module.exports = app;
 const api = require("./server.js");
 const timeInSeconds = 90 * 24 * 60 * 60;
-app.use(helmet.frameguard({ action: "deny" }));
-app.use(helmet.xssFilter());
-app.use(helmet.hidePoweredBy());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: timeInSeconds, force: true }));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
+// app.use(helmet.frameguard({ action: "deny" }));
+// app.use(helmet.xssFilter());
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts({ maxAge: timeInSeconds, force: true }));
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(
+//   helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] } })
+// );
 app.use(
-  helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] } })
+  helmet({
+    frameguard: {
+      action: "deny",
+    },
+    dnsPrefetchControl: false,
+    hidePoweredBy: true,
+    contentSecurityPolicy: {
+      // enable and configure
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["style.com"],
+      },
+    },
+  })
 );
 app.use(express.static("public"));
 app.disable("strict-transport-security");
