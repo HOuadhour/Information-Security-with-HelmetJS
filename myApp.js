6dfcb9fe016bba31cc857cf4cf9b751d8dd1d334
HOuadhour <HOuadhour@Yandex.com>
2021-07-21 20:51:36 +0100

    Disable Client-Side Caching with helmet.noCache()

--- myApp.js
+++ myApp.js
@@ -12,6 +12,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: timeInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 app.use(express.static("public"));
 app.disable("strict-transport-security");
 app.use("/_api", api);
